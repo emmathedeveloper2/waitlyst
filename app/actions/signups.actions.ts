6ac1a2231bd1e02bafedc70fc85fc4dd37c41bbd
emmathedeveloper2 <emmadavid063@gmail.com>
2025-06-25 11:52:15 +0100

    fix: enhance signup uniqueness check by adding appId to query

--- app/actions/signups.actions.ts
+++ app/actions/signups.actions.ts
@@ -1,5 +1,5 @@
 "use server";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { db } from "../_lib/db"
 import { signups } from "../_lib/db/schemas";
 
@@ -8,7 +8,7 @@ export async function addSignUp(email: string , appId: string , ownerId: string)
     try {
 
         const alreadyExists = await db.query.signups.findFirst({
-            where: eq(signups.email , email)
+            where: and(eq(signups.email , email) , eq(signups.appId , appId))
         })
 
         if(alreadyExists) throw Error("This email is already in the waitlist")
